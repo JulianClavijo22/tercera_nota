7a175e1b0f760ccab524d6f10d630e35b8652c52
Julian <andresjulian454k@gmai.com>
2025-10-22 20:37:01 -0500

    Se modifico la estructura del proyecto

--- src/Semana6/model/App.java
+++
@@ -1,9 +0,0 @@
-package Semana6.model;
-
-public class App {
-    public static void main(String[] args) {
-        Student studentTwo = new Student("juan", 4);
-        Student studentTree = new Student("Carlos", 5);
-    }
-    
-}
